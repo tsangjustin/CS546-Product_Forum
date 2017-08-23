4e47d555a8a04c51983a6b28fc20b4453ef1aaba
justint <justintcoding@gmail.com>
2017-08-23 15:50:41 -0400

    Add JS to send all search query to backend

--- public/js/communityForums.js
+++ public/js/communityForums.js
@@ -3,19 +3,46 @@
     var searchInput = $("#searchBox");
     var searchButton = $("#searhButton");
     var communityForumsList = $("#community-forums");
-    var searchParams = {};
     var fullCommunityForum = communityForumsList.html();
 
     searchInput.keyup(function(e) {
-        console.log(e.target.value);
+    });
+
+    searchButton.click(function(e) {
+        e.preventDefault();
         clearTimeout(searchTimeout);
         var url = "/forums/search?";
+        var searchParams = {};
+        var titleInput = searchInput.val().trim();
+        var checkedPrices = $('input[name="price[]"]:checked');
+        var checkedLabels = $('input[name="labels[]"]:checked');
+        // Add title/content search in query
+        if (titleInput.length > 0) {
+            searchParams.title = titleInput.toLowerCase();
+        }
+        // Add labels to query
+        if (checkedLabels.length > 0) {
+            searchParams.labels = [];
+            checkedLabels.each(function() {
+                searchParams.labels.push(this.value);
+            });
+        }
+        // Add prices to query
+        if (checkedPrices.length > 0) {
+            searchParams.price = [];
+            checkedPrices.each(function() {
+                searchParams.price.push(this.value);
+            });
+        }
+        console.log(searchParams);
         var queryString = "";
         var queryKeys = Object.keys(searchParams);
         for (q=0, lenKeys=queryKeys.length; q < lenKeys; ++q) {
             var currKey = queryKeys[q];
-            queryString += currKey + "=" + encodeURIComponent(searchParams[currKey].join(" ")) + "&";
+            queryString += currKey + "=" + ((Array.isArray(searchParams[currKey])) ? encodeURIComponent(searchParams[currKey].join(" ")) : searchParams[currKey]) + "&";
         }
+        checkedPrices
+        checkedPrices
         if (queryString.length <= 0) {
             communityForumsList.html(fullCommunityForum);
         } else {
@@ -32,9 +59,5 @@
                 });
             }, 1500);
         }
-    });
-
-    searchButton.click(function() {
-        
     });
 })(jQuery)
