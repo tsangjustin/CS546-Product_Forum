01c8b3726b7cebc4d955573eb634ca108116ff4a
justint <justintcoding@gmail.com>
2017-08-23 15:11:02 -0400

    Add submit click handler

--- public/js/communityForums.js
+++ public/js/communityForums.js
@@ -1,6 +1,7 @@
 (function($) {
     var searchTimeout = undefined;
     var searchInput = $("#searchBox");
+    var searchButton = $("#searhButton");
     var communityForumsList = $("#community-forums");
     var searchParams = {};
     var fullCommunityForum = communityForumsList.html();
@@ -8,24 +9,32 @@
     searchInput.keyup(function(e) {
         console.log(e.target.value);
         clearTimeout(searchTimeout);
-        searchTimeout = setTimeout(function() {
-            var url = "/forums/search?";
-            var queryString = ""
-            var queryKeys = Object.keys(searchParams);
-            for (q=0, lenKeys=queryKeys.length; q < lenKeys; ++q) {
-                var currKey = queryKeys[q];
-                queryString += currKey + "=" + encodeURIComponent(searchParams[currKey].join(" ")) + "&";
-            }
-            console.log(url);
-            $.ajax({
-                "url": url + queryString,
-                "type": "GET",
-                "dataType": 'html',
-                "success": function(res) {
-                    console.log(res);
-                    communityForumsList.innerHTML(res);
-                }
-            });
-        }, 1500);
+        var url = "/forums/search?";
+        var queryString = "";
+        var queryKeys = Object.keys(searchParams);
+        for (q=0, lenKeys=queryKeys.length; q < lenKeys; ++q) {
+            var currKey = queryKeys[q];
+            queryString += currKey + "=" + encodeURIComponent(searchParams[currKey].join(" ")) + "&";
+        }
+        if (queryString.length <= 0) {
+            communityForumsList.html(fullCommunityForum);
+        } else {
+            searchTimeout = setTimeout(function() {
+                console.log(url);
+                $.ajax({
+                    "url": url + queryString,
+                    "type": "GET",
+                    "dataType": 'html',
+                    "success": function(res) {
+                        console.log(res);
+                        communityForumsList.innerHTML(res);
+                    }
+                });
+            }, 1500);
+        }
+    });
+
+    searchButton.click(function() {
+        
     });
 })(jQuery)
