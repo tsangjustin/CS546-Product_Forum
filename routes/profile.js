cd580c3469ee52bc8bf7d5a6ad1b12f93a5516b0
justint <justintcoding@gmail.com>
2017-08-24 09:03:09 -0400

    Add update endpoint for profile that renders new profile if valid edit

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+// Custom Node Helper
+const usersData = require('../data/').user;
 
 router.get('/', (req, res) => {
 	if (!req.user) {
-		return res.redirect('/');
+		return res.redirect('/log_in');
 	}
 	const userInfo = {
 		avatar: req.user.avatar,
@@ -12,6 +14,35 @@ router.get('/', (req, res) => {
 	};
 	console.log(userInfo);
 	return res.render('profile/profile', userInfo);
+});
+
+router.get('/edit/', (req, res) => {
+    if (!req.user) {
+		return res.redirect('/log_in');
+	}
+    const userInfo = {
+		avatar: req.user.avatar,
+		username: req.user.username,
+		email: req.user.email,
+		gender: (req.user.isMale),
+        layout: false,
+	};
+    console.log(userInfo);
+    return res.render('profile/editProfile.handlebars', userInfo);
+});
+
+router.post('/edit/', (req, res) => {
+    if (!req.user) {
+		return res.redirect('/log_in');
+	}
+    usersData.updateUser(req.user._id, req.body).then((newProfile) => {
+        newProfile.gender = (newProfile.isMale) ? "Male" : "Female",
+        newProfile.layout = false;
+        return res.render('profile/profile.handlebars', newProfile);
+    }).catch((err) => {
+        console.log(err);
+        return res.status(500).json({error: err});
+    });
 });
 
 module.exports = exports = router;
